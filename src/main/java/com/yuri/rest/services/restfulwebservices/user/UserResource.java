8d3430a90b526bd079314aac9b5b23bd52c26771
yunishimura0716 <yunishimura0716@gmail.com>
2022-04-26 21:54:41 -0700

    added delete user method

--- src/main/java/com/yuri/rest/services/restfulwebservices/user/UserResource.java
+++ src/main/java/com/yuri/rest/services/restfulwebservices/user/UserResource.java
@@ -39,4 +39,12 @@ public class UserResource {
         URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId()).toUri();
         return ResponseEntity.created(location).build();
     }
+
+    @DeleteMapping("/users/{id}")
+    public void deleteUser(@PathVariable int id) {
+        User user = service.deleteById(id);
+        if (user == null) {
+            throw new UserNotFoundException("id-" + id);
+        }
+    }
 }
